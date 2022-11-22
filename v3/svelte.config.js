1780f8a481affa328f37e849a71d47fa41010555
Hrvoje Bartolin <hbartolin@gmail.com>
2022-11-22 17:42:52 +0100

    Update svelte.config.js

--- v3/svelte.config.js
+++ v3/svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-static' /* @sveltejs/adapter-auto' */;
 import preprocess from 'svelte-preprocess';
-//const dev = process.argv.includes('dev');
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
