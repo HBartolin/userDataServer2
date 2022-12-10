cd9f7959220cd3a7a3c2220025e21c449bcf3374
Hrvoje Bartolin <hbartolin@gmail.com>
2022-12-10 21:23:39 +0100

    7

--- v3/src/routes/about/+page.svelte
+++ v3/src/routes/about/+page.svelte
@@ -1,10 +1,16 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	console.log($page.url.searchParams.get('id'));
+</script>
+
 <svelte:head>
 	<title>About</title>
 	<meta name="description" content="About this app" />
 </svelte:head>
 
 <div class="text-column">
-	<h1>About this app</h1>
+	<h1>11111111111111111 About this app</h1>
 
 	<p>
 		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
