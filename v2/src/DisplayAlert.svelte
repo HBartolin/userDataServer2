ebfb85a04210c802631ef5db85ccc4f6331817c6
Hrvoje Bartolin <hbartolin@gmail.com>
2023-01-07 14:42:50 +0100

    del

--- v2/src/DisplayAlert.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    export let msg = "Nedostaje poruka.";
-</script>
-
-<div class="alert alert-success position-fixed" style="z-index: 1" role="alert">
-    {msg}
-</div>
-	
-
--- v2/svelte.config.js
+++
@@ -1,8 +0,0 @@
-import sveltePreprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
-
-export default {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: sveltePreprocess()
-}
