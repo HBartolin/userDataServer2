fc8c2bfcd7b751120c2d2fc4933a3dfe6d809414
Hrvoje Bartolin <hbartolin@gmail.com>
2023-03-19 07:20:07 +0100

    2

--- lib/src/main/java/hr/bart/userDataServer/DefaultView.java
+++ lib/src/main/java/hr/bart/userDataServer/DefaultView.java
@@ -1,21 +1,71 @@
 package hr.bart.userDataServer;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class DefaultView extends WebMvcConfigurerAdapter {
+	private static final String INDEX_HTML = "index.html";
+    private static final String _INDEX_HTML = "/" + INDEX_HTML;
 
-    @Override
-    public void addViewControllers( ViewControllerRegistry registry ) {
+	@Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        File file;
+        try {
+            file = new ClassPathResource("static").getFile();
+            String[] names = file.list();
+
+            System.out.println("--------------------------------------------");
+            for(String name : names) {
+            	File isFolder=new File(file.getAbsolutePath() + "/" + name);
+            	
+                if (isFolder.isDirectory()) {
+                	if(new File(isFolder, INDEX_HTML).exists()) {
+                		registry.addViewController("/" + name).setViewName("forward:/" + name +_INDEX_HTML);
+                    	System.out.println("/" + name + " -> " + "forward:/" + name +_INDEX_HTML);
+                    	
+                    	skenDeeper(registry, isFolder, name);
+                	}
+                }
+            }
+            System.out.println("--------------------------------------------");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    /*public void addViewControllers( ViewControllerRegistry registry ) {
         registry.addViewController( "/v1" ).setViewName( "forward:/v1/index.html" );
         registry.addViewController( "/v1/" ).setViewName( "forward:/v1/index.html" );
 //        registry.addViewController( "/svelteKit" ).setViewName( "forward:/svelteKit/index.html" );
         registry.addViewController( "/svelteKit/" ).setViewName( "forward:/svelteKit/index.html" );
+//        registry.addViewController( "/svelteKit/about/" ).setViewName( "forward:/svelteKit/about/index.html" );
         registry.addViewController( "/svelteKit/about2/" ).setViewName( "forward:/svelteKit/about2/index.html" );
         registry.setOrder( Ordered.HIGHEST_PRECEDENCE );
         super.addViewControllers( registry );
-    }
+    }*/
+
+	private void skenDeeper(ViewControllerRegistry registry, File isFolder, String name) {
+        File[] list = isFolder.listFiles();
+		
+        if (list == null) return;
+        
+        for ( File f : list ) {
+        	if ( f.isDirectory() ) {
+        		if(new File(f, INDEX_HTML).exists()) {
+        			String odS="/" + name + "/" + f.getName();
+        			String doS="forward:/" + name + "/" + f.getName() + _INDEX_HTML;
+        			
+            		registry.addViewController(odS).setViewName(doS);
+                	System.out.println(odS + " -> " + doS);
+                	
+                	skenDeeper(registry, f, odS.substring(1));
+            	}
+        	}
+        }
+	}
 }
--- lib/src/main/java/hr/bart/userDataServer/UserDataServerApplication.java
+++ lib/src/main/java/hr/bart/userDataServer/UserDataServerApplication.java
@@ -2,16 +2,18 @@ package hr.bart.userDataServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import co.elastic.apm.attach.ElasticApmAttacher;
 
 @SpringBootApplication
+//@ComponentScan
 public class UserDataServerApplication {
 
 	public static void main(String[] args) {
 //		ElasticApmAttacher.attach();
 		ApplicationContext ctx =SpringApplication.run(UserDataServerApplication.class, args);
-		String[] beans = ctx.getBeanDefinitionNames();
+//		String[] beans = ctx.getBeanDefinitionNames();
 		
 //		for(String s: beans) {
 //			System.out.println(s);
