890ff356ea508a8c6ecf62803fcd5d8b0a2dd060
Hrvoje Bartolin <hbartolin@gmail.com>
2023-11-23 06:49:56 +0100

    2

--- lib/src/main/java/hr/bart/userDataServer/service/kod/ProjektDetaljiServiceImplProjektDatalji.java
+++ lib/src/main/java/hr/bart/userDataServer/service/kod/ProjektDetaljiServiceImplProjektDatalji.java
@@ -3,38 +3,25 @@ package hr.bart.userDataServer.service.kod;
 import java.util.HashMap;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import hr.bart.userDataServer.db.ProjektDetalji;
-import hr.bart.userDataServer.repository.ProjektDetaljiRepository;
-import hr.bart.userDataServer.repository.ProjektRepository;
 import hr.bart.userDataServer.util.PojoInterface;
 
 public class ProjektDetaljiServiceImplProjektDatalji extends Kod {
 	private Long id=(Long) hm.get("id");
-	@Autowired
-	private ProjektDetaljiRepository projektDetaljiRepository;
 
 	public ProjektDetaljiServiceImplProjektDatalji(HashMap<String, Object> hm, KodRepository kodRepository) {
 		super(hm, kodRepository);
 	}
-	
-	public ProjektDetaljiServiceImplProjektDatalji(HashMap<String, Object> hm) {
-		super(hm);
-	}
 
 	@Override
 	public PojoInterface izvrsiKod(PojoInterface pi) throws Throwable {
-		System.out.println("!!!! " + projektDetaljiRepository);
-		Optional<ProjektDetalji> projektDetalji=projektDetaljiRepository.findById(id);
+		Optional<ProjektDetalji> projektDetalji=kodRepository.getProjektDetaljiRepository().findById(id);
 		
 		if(projektDetalji.isPresent()) {
 			pi.setRezultat(projektDetalji.get());
 		} else {
 			pi.setGreska(String.format("Ne postoji podatak za ProjektDetalji=%d.", id));
 		}
-		
-		
 		
 		return pi;
 	}
