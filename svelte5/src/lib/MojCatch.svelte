7d496dc532e813a117955f8fd0d277ac545831ce
Hrvoje Bartolin <hbartolin@gmail.com>
2024-09-06 18:37:39 +0200

    1

--- svelte5/src/lib/MojCatch.svelte
+++ svelte5/src/lib/MojCatch.svelte
@@ -1,22 +1,9 @@
 <script lang="ts">
   import BartToast from "./BartToast.svelte";
-//    import { onMount } from 'svelte';
- //   import { SvelteToast, toast } from '@zerodevx/svelte-toast'
 
-    export let errorMsg: string = "Nedostaje argument 'errorMsg'.";
-    export let durationMS: number = 100000;
+   let {errorMsg, durationMS} = $props();
 
-    $: console.log(errorMsg);
-
-//    onMount(() => {
-   //     toast.pop(0);
-   //     toast.push(errorMsg.toString());
- //   });
-
-   // const options = {
-     //   'duration': durationMS,
-  //  };
-    // <SvelteToast {options} />  class="wrap-red"
+   console.log(errorMsg);
 </script>
 
 <BartToast msg={errorMsg} alertMsg="alert-error" />
