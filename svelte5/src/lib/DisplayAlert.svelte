bf1ddf7e17df6a28c4f0447bb66a0dfdd86012ef
Hrvoje Bartolin <hbartolin@gmail.com>
2024-09-06 18:35:14 +0200

    1

--- svelte5/src/lib/DisplayAlert.svelte
+++ svelte5/src/lib/DisplayAlert.svelte
@@ -1,19 +1,7 @@
 <script lang="ts">
     import BartToast from "./BartToast.svelte";
-    //import { onMount } from 'svelte';
-   // import { SvelteToast, toast } from '@zerodevx/svelte-toast'
 
-    export let msg: string = "Nedostaje poruka.";
-
-  //  onMount(() => {
-   //    toast.pop(0);
-   //    toast.push(msg);
-  //  })
-
-    //const options = {
-    //};
-
-    // <SvelteToast {options} />
+    let {msg} = $props();
 </script>
 	
 <BartToast {msg} alertMsg="alert-success" />
