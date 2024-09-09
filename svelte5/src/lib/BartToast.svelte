88ac71f8adc4bd8f5b7cd0e90a0865747a8a94a2
Hrvoje Bartolin <hbartolin@gmail.com>
2024-09-09 22:25:26 +0200

    1

--- svelte5/src/lib/BartToast.svelte
+++ svelte5/src/lib/BartToast.svelte
@@ -2,7 +2,7 @@
      let {msg, alertMsg}=$props();
 </script>
 
-<div class="toast toast-center toast-middle">
+<div class="toast toast-center top-40">
     <div class="alert {alertMsg}">
       <span>{msg}</span>
     </div>
