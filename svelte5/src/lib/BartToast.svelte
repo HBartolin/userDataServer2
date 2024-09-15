c9841d246155089e166700ccb8a2f98d9912d9a1
Hrvoje Bartolin <hbartolin@gmail.com>
2024-09-15 15:12:27 +0200

    4

--- svelte5/src/lib/BartToast.svelte
+++ svelte5/src/lib/BartToast.svelte
@@ -1,9 +1,38 @@
 <script lang="ts">
   let { msg, alertMsg } = $props();
+
+  let showModal = $state(true);
 </script>
 
-<div class="toast toast-center toast-top z-50">
-  <div class="alert {alertMsg}">
-    <span>{msg}</span>
+{#if showModal}
+  <div class="toast toast-center toast-top z-50">
+    <div id="bartToast" class="alert {alertMsg}">
+      <span>{msg}</span>
+
+      <button
+        type="button"
+        class="close-btn"
+        onclick={() => (showModal = false)}
+      >
+        <svg
+          class="w-3 h-3"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 14"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+          />
+        </svg>
+      </button>
+    </div>
   </div>
-</div>
+{/if}
+
+<style>
+</style>
