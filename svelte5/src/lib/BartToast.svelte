6d017a5932b2c62f62c324b2cd4b03879a038f57
Hrvoje Bartolin <hbartolin@gmail.com>
2024-09-16 20:06:16 +0200

    1

--- svelte5/src/lib/BartToast.svelte
+++ svelte5/src/lib/BartToast.svelte
@@ -5,28 +5,17 @@
 </script>
 
 {#if showModal}
-  <div class="toast toast-center toast-top z-50 mt-24 min-w-80 max-w-96">
-    <div class="alert {alertMsg} text-wrap">
-      <span>{msg}</span>
+  <div class="toast toast-center toast-top z-50">
+    <div class="alert {alertMsg}">
+      {msg}
 
       <button type="button" onclick={() => (showModal = false)}>
-        <svg
-          class="w-3 h-3"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 14 14">
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-        </svg>
+        X
       </button>
     </div>
   </div>
 {/if}
 
 <style>
+  .nnoowhite-space {white-space: pre-line;}
 </style>
